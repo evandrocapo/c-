a7b0b9646e3add43cf5e6f6a8958b856bf3bb4d0
Evandro Capovilla <evandro.junior@venturus.org.br>
2020-03-19 11:03:18 -0300

    initial commit

---
+++ Alura Course 2/Alura Course 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Alura Course 2
+//
+//  Created by Evandro Douglas Capovilla Junior on 19/03/20.
+//  Copyright © 2020 Evandro Douglas Capovilla Junior. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
